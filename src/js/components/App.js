829bba6d455e34a0149116fa34ed4c643d870e5f
Michael Luckyman <lukiman@quorum.us>
2020-02-22 21:44:35 -0500

    add right name

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -3,8 +3,11 @@ import List from "./List"
 
 const App = () => (
     <div>
-        <h2>Articles</h2>
+        <h2>b14n</h2>
             <List/>
+            <h3>About Us</h3>
+            <p>Have you ever wanted to see your dreams come to life? We can help.</p>
+            <h3>Get a Quote</h3>
     </div>
 )
 
